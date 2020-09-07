6a04357dbddfa0b1fbe857f3b49ab7ecc80e9b66
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2020-09-07 11:30:38 -0300

    Att Chat

---
+++ lib/routes/chat/Chat.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import './Visualizacao.dart';
+
+class Chat extends StatefulWidget {
+  Chat({Key key, this.id_user}) : super(key: key);
+  final int id_user;
+  @override
+  _ChatState createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  List grupos = [
+    {
+      "nome": "Dúvidas Python",
+      "qts_user": "10",
+      "imagem": "assets/python.png",
+      "descricao": "Grupo para tirar dúvidas de Python no geral"
+    },
+    {
+      "nome": "Dúvidas R",
+      "qts_user": "8",
+      "imagem": "assets/r.jpeg",
+      "descricao": "Grupo para tirar dúvidas de R no geral"
+    },
+    {
+      "nome": "Dúvidas Machine Learning",
+      "qts_user": "10",
+      "imagem": "assets/machine.png",
+      "descricao":
+          "Grupo para Dúvidas de Machine Learning(Scikit-learn, KNN, ...)"
+    },
+    {
+      "nome": "Dúvidas Deep Learning",
+      "qts_user": "10",
+      "imagem": "assets/deep.png",
+      "descricao": "Grupo para Dúvidas de Deep Learning(Tensorflow, spark, ...)"
+    },
+    {
+      "nome": "Networking",
+      "qts_user": "10",
+      "imagem": "assets/social.png",
+      "descricao": "Grupo para ajudar você a construir seu Networking :)"
+    },
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    print("----------------- CHAT.DART -----------------");
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: grupos.length,
+        itemBuilder: (context, index) {
+          return SizedBox(
+              height: 90,
+              child: Card(
+                child: ListTile(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Visualizacao(
+                                  titulo: grupos[index]["nome"],
+                                  imagem: grupos[index]["imagem"],
+                                  id_user: widget.id_user,
+                                ))),
+                    title: Text(grupos[index]["nome"]),
+                    subtitle: Text(grupos[index]["descricao"]),
+                    leading: Padding(
+                      padding: EdgeInsets.only(top: 10),
+                      child: ClipRRect(
+                        child: Image.asset(
+                          grupos[index]["imagem"],
+                          width: 50,
+                        ),
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    )),
+              ));
+        });
+  }
+}
---
+++ lib/routes/chat/Visualizacao.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:mysql1/mysql1.dart' as mysql;
+import 'dart:async';
+
+class Visualizacao extends StatefulWidget {
+  Visualizacao({Key key, this.titulo, this.imagem, this.id_user})
+      : super(key: key);
+  final String titulo;
+  final String imagem;
+  final int id_user;
+
+  @override
+  _VisualizacaoState createState() => _VisualizacaoState();
+}
+
+class _VisualizacaoState extends State<Visualizacao> {
+  String texto;
+  Map mensagem;
+  List mensagens = [];
+
+  var config = mysql.ConnectionSettings();
+
+  Future getMensagens() async {
+    var conn = await mysql.MySqlConnection.connect(config);
+    mensagens = [];
+    try {
+      var results = await conn
+          .query("select * from mensagens")
+          .then((value) => value.forEach((element) {
+                try {
+                  //print("'Visualizacao.dart': Mensagem: ${element["conteudo"]}");
+                  mensagem = {
+                    "nome": element["conteudo"],
+                    "id_user": element["id_user"]
+                  };
+                  mensagens.add(mensagem);
+                } catch (e) {
+                  print("'Visualizacao.dart': Não foi possível add");
+                }
+              }));
+      if (mensagens.length == 0) {
+        print("'Visualizacao.dart': Sem nenhuma mensagem");
+      } else {
+        print("'Visualizacao.dart': Com mensagem");
+        conn.close();
+        return mensagens;
+      }
+    } catch (e) {
+      print(e);
+    }
+    mensagens = [];
+  }
+
+  Future addMensagens(content, id_user) async {
+    var conn = await mysql.MySqlConnection.connect(config);
+    try {
+      var results = await conn.query(
+          "insert into mensagens values (null, ?, ?)", [content, id_user]);
+      print("'Visualizacao.dart': Texto adicionado $content");
+      //await getMensagens();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void resetar() async {
+    setState(() {
+      mensagem = {};
+      mensagens = [];
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    print("----------------- VISUALIZACAO.DART -----------------");
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            ClipRRect(
+              child: Image.asset(
+                "${widget.imagem}",
+                height: 45,
+                filterQuality: FilterQuality.high,
+                fit: BoxFit.fill,
+                semanticLabel: "Imagem Grupo",
+              ),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: Text("${widget.titulo}"))
+          ],
+        ),
+      ),
+      body: FutureBuilder(
+          future: getMensagens(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Padding(
+                  padding: EdgeInsets.only(bottom: 50),
+                  child: ListView.builder(
+                      itemCount: mensagens.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(mensagens[index]["nome"]),
+                          subtitle: Text("$index"),
+                        );
+                      }));
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+      bottomSheet: Row(children: [
+        Container(
+          padding: EdgeInsets.only(left: 10, bottom: 1),
+          width: 340.0,
+          child: TextField(
+            onChanged: (value) {
+              texto = value;
+              print("'Visualizacao.dart': Texto: $texto");
+            },
+            decoration: InputDecoration(
+                labelText: "Digite aqui sua dúvida",
+                floatingLabelBehavior: FloatingLabelBehavior.never),
+          ),
+        ),
+        IconButton(
+            icon: Icon(
+              Icons.message,
+              color: Colors.red,
+            ),
+            onPressed: () async {
+              setState(() async {
+                await addMensagens(texto, widget.id_user);
+                await resetar();
+              });
+            })
+      ]),
+    );
+  }
+}
